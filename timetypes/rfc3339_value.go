ff8974cee9417c9540834ac36f9c86be324e32e3
Selena Goods <github@simplebox.anonaddy.com>
2023-07-27 15:34:52 -0400

    Implement `RFC3339Type` and `RFC3339`

---
+++ timetypes/rfc3339_value.go
@@ -0,0 +1,125 @@
+package timetypes
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+)
+
+var (
+	_ basetypes.StringValuable = (*RFC3339)(nil)
+)
+
+// RFC3339 represents a valid RFC3339-formatted string. Semantic equality logic is defined for RFC3339
+// such that inconsequential differences between the `Z` suffix and a `00:00` UTC offset are ignored.
+type RFC3339 struct {
+	basetypes.StringValue
+}
+
+// Type returns an RFC3339Type.
+func (v RFC3339) Type(_ context.Context) attr.Type {
+	return RFC3339Type{}
+}
+
+// Equal returns true if the given value is equivalent.
+func (v RFC3339) Equal(o attr.Value) bool {
+	other, ok := o.(RFC3339)
+
+	if !ok {
+		return false
+	}
+
+	return v.StringValue.Equal(other.StringValue)
+}
+
+// StringSemanticEquals returns true if the given RFC3339 string value is semantically equal to the current RFC3339 string value.
+// This comparison utilizes time.Parse to create time.Time instances and then compares the resulting RFC 3339-formatted string representations.
+// This means RFC3339 values utilizing the `Z` Zulu suffix as an offset are considered semantically equal to RFC3339 that define a `00:00` UTC offset.
+//
+// Examples:
+//   - `2023-07-25T20:43:16+00:00` is semantically equal to `2023-07-25T20:43:16Z`
+//   - `2023-07-25T20:43:16-00:00` is semantically equal to `2023-07-25T20:43:16Z` - while RFC 3339 defines an unknown local
+//     offset (`-00:00`) to be different from an offset of `Z`, time.Parse converts `-00:00` to `+00:00` during parsing.
+//
+// Counterexamples:
+//   - `2023-07-25T23:43:16+00:00` expresses the same time as `2023-07-25T20:43:16-03:00` but is NOT considered
+//     to be semantically equal.
+//
+// See RFC 3339 for more details on the string format: https://www.rfc-editor.org/rfc/rfc3339.html.
+func (v RFC3339) StringSemanticEquals(_ context.Context, newValuable basetypes.StringValuable) (bool, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	newValue, ok := newValuable.(RFC3339)
+	if !ok {
+		diags.AddError(
+			"Semantic Equality Check Error",
+			"An unexpected value type was received while performing semantic equality checks. "+
+				"Please report this to the provider developers.\n\n"+
+				"Expected Value Type: "+fmt.Sprintf("%T", v)+"\n"+
+				"Got Value Type: "+fmt.Sprintf("%T", newValuable),
+		)
+
+		return false, diags
+	}
+
+	// RFC3339 strings are already validated at this point, ignoring errors
+	newRFC3339time, _ := time.Parse(time.RFC3339, newValue.ValueString())
+	currentRFC3339time, _ := time.Parse(time.RFC3339, v.ValueString())
+
+	return currentRFC3339time.Format(time.RFC3339) == newRFC3339time.Format(time.RFC3339), diags
+}
+
+// ValueRFC3339Time creates a new time.Time instance with the RFC3339 StringValue. A null or unknown value will produce an error diagnostic.
+func (v RFC3339) ValueRFC3339Time() (time.Time, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	if v.IsNull() {
+		diags.Append(diag.NewErrorDiagnostic("RFC3339 ValueRFC3339Time Error", "RFC3339 string value is null"))
+		return time.Time{}, diags
+	}
+
+	if v.IsUnknown() {
+		diags.Append(diag.NewErrorDiagnostic("RFC3339 ValueRFC3339Time Error", "RFC3339 string value is unknown"))
+		return time.Time{}, diags
+	}
+
+	rfc3339Time, err := time.Parse(time.RFC3339, v.ValueString())
+	if err != nil {
+		diags.Append(diag.NewErrorDiagnostic("RFC3339 ValueRFC3339Time Error", err.Error()))
+		return time.Time{}, diags
+	}
+
+	return rfc3339Time, nil
+}
+
+// NewRFC3339Null creates an RFC3339 with a null value. Determine whether the value is null via IsNull method.
+func NewRFC3339Null() RFC3339 {
+	return RFC3339{
+		StringValue: basetypes.NewStringNull(),
+	}
+}
+
+// NewRFC3339Unknown creates an RFC3339 with an unknown value. Determine whether the value is unknown via IsUnknown method.
+func NewRFC3339Unknown() RFC3339 {
+	return RFC3339{
+		StringValue: basetypes.NewStringUnknown(),
+	}
+}
+
+// NewRFC3339Value creates an RFC3339 with a known value. Access the value via ValueString method.
+func NewRFC3339Value(value string) RFC3339 {
+	return RFC3339{
+		StringValue: basetypes.NewStringValue(value),
+	}
+}
+
+// NewRFC3339PointerValue creates an RFC3339 with a null value if nil or a known value. Access the value via ValueStringPointer method.
+func NewRFC3339PointerValue(value *string) RFC3339 {
+	return RFC3339{
+		StringValue: basetypes.NewStringPointerValue(value),
+	}
+}
---
+++ timetypes/rfc3339_value_test.go
@@ -0,0 +1,153 @@
+package timetypes_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
+)
+
+func TestRFC3339_StringSemanticEquals(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		currentRFC3339time timetypes.RFC3339
+		givenRFC3339time   basetypes.StringValuable
+		expectedMatch      bool
+		expectedDiags      diag.Diagnostics
+	}{
+		"not equal - different dates": {
+			currentRFC3339time: timetypes.NewRFC3339Value("2023-07-25T23:43:16Z"),
+			givenRFC3339time:   timetypes.NewRFC3339Value("2023-07-26T23:43:16Z"),
+			expectedMatch:      false,
+		},
+		"not equal - different times": {
+			currentRFC3339time: timetypes.NewRFC3339Value("2023-07-25T23:43:16Z"),
+			givenRFC3339time:   timetypes.NewRFC3339Value("2023-07-25T23:01:16Z"),
+			expectedMatch:      false,
+		},
+		"not equal - different offset times": {
+			currentRFC3339time: timetypes.NewRFC3339Value("2023-07-25T23:43:16Z"),
+			givenRFC3339time:   timetypes.NewRFC3339Value("2023-07-25T23:43:16+03:00"),
+			expectedMatch:      false,
+		},
+		"not equal - UTC time and local time": {
+			currentRFC3339time: timetypes.NewRFC3339Value("2023-07-25T23:43:16Z"),
+			givenRFC3339time:   timetypes.NewRFC3339Value("2023-07-25T20:43:16-03:00"),
+			expectedMatch:      false,
+		},
+		"semantically equal - Z suffix and positive zero num offset": {
+			currentRFC3339time: timetypes.NewRFC3339Value("2023-07-25T23:43:16Z"),
+			givenRFC3339time:   timetypes.NewRFC3339Value("2023-07-25T23:43:16+00:00"),
+			expectedMatch:      true,
+		},
+		"semantically equal - Z suffix and negative zero num offset": {
+			currentRFC3339time: timetypes.NewRFC3339Value("2023-07-25T23:43:16Z"),
+			givenRFC3339time:   timetypes.NewRFC3339Value("2023-07-25T23:43:16-00:00"),
+			expectedMatch:      true,
+		},
+		"semantically equal - negative zero and positive zero num offset": {
+			currentRFC3339time: timetypes.NewRFC3339Value("2023-07-25T23:43:16-00:00"),
+			givenRFC3339time:   timetypes.NewRFC3339Value("2023-07-25T23:43:16+00:00"),
+			expectedMatch:      true,
+		},
+		"semantically equal - byte for byte match": {
+			currentRFC3339time: timetypes.NewRFC3339Value("2023-07-25T23:43:16Z"),
+			givenRFC3339time:   timetypes.NewRFC3339Value("2023-07-25T23:43:16Z"),
+			expectedMatch:      true,
+		},
+		"error - not given RFC3339 value": {
+			currentRFC3339time: timetypes.NewRFC3339Value("0000-00-00T00:00:00-00:00"),
+			givenRFC3339time:   basetypes.NewStringValue("0000-00-00T00:00:00-00:00"),
+			expectedMatch:      false,
+			expectedDiags: diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"Semantic Equality Check Error",
+					"An unexpected value type was received while performing semantic equality checks. "+
+						"Please report this to the provider developers.\n\n"+
+						"Expected Value Type: timetypes.RFC3339\n"+
+						"Got Value Type: basetypes.StringValue",
+				),
+			},
+		},
+	}
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			match, diags := testCase.currentRFC3339time.StringSemanticEquals(context.Background(), testCase.givenRFC3339time)
+
+			if testCase.expectedMatch != match {
+				t.Errorf("Expected StringSemanticEquals to return: %t, but got: %t", testCase.expectedMatch, match)
+			}
+
+			if diff := cmp.Diff(diags, testCase.expectedDiags); diff != "" {
+				t.Errorf("Unexpected diagnostics (-got, +expected): %s", diff)
+			}
+		})
+	}
+}
+
+func TestRFC3339_ValueRFC3339Time(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		RFC3339           timetypes.RFC3339
+		expectedTimestamp string
+		expectedDiags     diag.Diagnostics
+	}{
+		"RFC3339 string value is null ": {
+			RFC3339: timetypes.NewRFC3339Null(),
+			expectedDiags: diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"RFC3339 ValueRFC3339Time Error",
+					"RFC3339 string value is null",
+				),
+			},
+		},
+		"RFC3339 string value is unknown ": {
+			RFC3339: timetypes.NewRFC3339Unknown(),
+			expectedDiags: diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"RFC3339 ValueRFC3339Time Error",
+					"RFC3339 string value is unknown",
+				),
+			},
+		},
+		"valid RFC3339 Timestamp - Zulu suffix": {
+			RFC3339:           timetypes.NewRFC3339Value("2023-07-25T23:43:16Z"),
+			expectedTimestamp: "2023-07-25T23:43:16Z",
+		},
+		"valid RFC3339 Timestamp - UTC offset ": {
+			RFC3339:           timetypes.NewRFC3339Value("2023-07-25T23:43:16-00:00"),
+			expectedTimestamp: "2023-07-25T23:43:16-00:00",
+		},
+		"valid RFC3339 Timestamp - EDT offset ": {
+			RFC3339:           timetypes.NewRFC3339Value("2023-07-25T23:43:16-04:00"),
+			expectedTimestamp: "2023-07-25T23:43:16-04:00",
+		},
+	}
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			rfc3339Time, diags := testCase.RFC3339.ValueRFC3339Time()
+			expectedRFC3339Time, _ := time.Parse(time.RFC3339, testCase.expectedTimestamp)
+
+			if rfc3339Time != expectedRFC3339Time {
+				t.Errorf("Unexpected difference in time.Time, got: %s, expected: %s", rfc3339Time, expectedRFC3339Time)
+			}
+
+			if diff := cmp.Diff(diags, testCase.expectedDiags); diff != "" {
+				t.Errorf("Unexpected diagnostics (-got, +expected): %s", diff)
+			}
+		})
+	}
+}
