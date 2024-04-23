72c8c79d6f06d5809e69c705a9ca0fcc82952454
Benjamin Bennett <ben.bennett@hashicorp.com>
2024-04-23 09:55:29 +0100

    timetypes: Replace usage of deprecated xattr.TypeWithValue interface with xattr.ValidateableAttribute and function.ValidateableParameter (#54)

--- timetypes/rfc3339_value_test.go
+++ timetypes/rfc3339_value_test.go
@@ -9,7 +9,10 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/attr/xattr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/function"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 
 	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
@@ -91,6 +94,186 @@ func TestRFC3339_StringSemanticEquals(t *testing.T) {
 			}
 
 			if diff := cmp.Diff(diags, testCase.expectedDiags); diff != "" {
+				t.Errorf("Unexpected diagnostics (-got, +expected): %s", diff)
+			}
+		})
+	}
+}
+
+func TestRFC3339ValidateAttribute(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		RFC3339       timetypes.RFC3339
+		expectedDiags diag.Diagnostics
+	}{
+		"empty-struct": {
+			RFC3339: timetypes.RFC3339{},
+		},
+		"null": {
+			RFC3339: timetypes.NewRFC3339Null(),
+		},
+		"unknown": {
+			RFC3339: timetypes.NewRFC3339Unknown(),
+		},
+		"valid RFC3339": {
+			RFC3339: timetypes.NewRFC3339ValueMust("2023-07-25T20:43:16+00:00"),
+		},
+		"valid RFC3339 - Zulu": {
+			RFC3339: timetypes.NewRFC3339ValueMust("2023-07-25T20:43:16Z"),
+		},
+		"valid RFC3339 - UTC Offset": {
+			RFC3339: timetypes.NewRFC3339ValueMust("2023-07-25T20:43:16-05:00"),
+		},
+		"invalid RFC3339 - no date": {
+			RFC3339: timetypes.RFC3339{
+				StringValue: basetypes.NewStringValue("20:43:16-05:00"),
+			},
+			expectedDiags: diag.Diagnostics{
+				diag.NewAttributeErrorDiagnostic(
+					path.Root("test"),
+					"Invalid RFC3339 String Value",
+					"A string value was provided that is not valid RFC3339 string format.\n\n"+
+						"Given Value: 20:43:16-05:00\n"+
+						"Error: parsing time \"20:43:16-05:00\" as \"2006-01-02T15:04:05Z07:00\": "+
+						"cannot parse \"20:43:16-05:00\" as \"2006\"",
+				),
+			},
+		},
+		"invalid RFC3339 - no time": {
+			RFC3339: timetypes.RFC3339{
+				StringValue: basetypes.NewStringValue("2023-07-25T"),
+			},
+			expectedDiags: diag.Diagnostics{
+				diag.NewAttributeErrorDiagnostic(
+					path.Root("test"),
+					"Invalid RFC3339 String Value",
+					"A string value was provided that is not valid RFC3339 string format.\n\n"+
+						"Given Value: 2023-07-25T\n"+
+						"Error: parsing time \"2023-07-25T\" as \"2006-01-02T15:04:05Z07:00\": "+
+						"cannot parse \"\" as \"15\"",
+				),
+			},
+		},
+		"invalid RFC3339 - normal string": {
+			RFC3339: timetypes.RFC3339{
+				StringValue: basetypes.NewStringValue("notvalidrfc3339"),
+			},
+			expectedDiags: diag.Diagnostics{
+				diag.NewAttributeErrorDiagnostic(
+					path.Root("test"),
+					"Invalid RFC3339 String Value",
+					"A string value was provided that is not valid RFC3339 string format.\n\n"+
+						"Given Value: notvalidrfc3339\n"+
+						"Error: parsing time \"notvalidrfc3339\" as \"2006-01-02T15:04:05Z07:00\": "+
+						"cannot parse \"notvalidrfc3339\" as \"2006\"",
+				),
+			},
+		},
+	}
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			resp := xattr.ValidateAttributeResponse{}
+
+			testCase.RFC3339.ValidateAttribute(
+				context.Background(),
+				xattr.ValidateAttributeRequest{
+					Path: path.Root("test"),
+				},
+				&resp,
+			)
+
+			if diff := cmp.Diff(resp.Diagnostics, testCase.expectedDiags); diff != "" {
+				t.Errorf("Unexpected diagnostics (-got, +expected): %s", diff)
+			}
+		})
+	}
+}
+
+func TestRFC3339ValidateParameter(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		RFC3339         timetypes.RFC3339
+		expectedFuncErr *function.FuncError
+	}{
+		"empty-struct": {
+			RFC3339: timetypes.RFC3339{},
+		},
+		"null": {
+			RFC3339: timetypes.NewRFC3339Null(),
+		},
+		"unknown": {
+			RFC3339: timetypes.NewRFC3339Unknown(),
+		},
+		"valid RFC3339": {
+			RFC3339: timetypes.NewRFC3339ValueMust("2023-07-25T20:43:16+00:00"),
+		},
+		"valid RFC3339 - Zulu": {
+			RFC3339: timetypes.NewRFC3339ValueMust("2023-07-25T20:43:16Z"),
+		},
+		"valid RFC3339 - UTC Offset": {
+			RFC3339: timetypes.NewRFC3339ValueMust("2023-07-25T20:43:16-05:00"),
+		},
+		"invalid RFC3339 - no date": {
+			RFC3339: timetypes.RFC3339{
+				StringValue: basetypes.NewStringValue("20:43:16-05:00"),
+			},
+			expectedFuncErr: function.NewArgumentFuncError(
+				0,
+				"Invalid RFC3339 String Value: "+
+					"A string value was provided that is not valid RFC3339 string format.\n\n"+
+					"Given Value: 20:43:16-05:00\n"+
+					"Error: parsing time \"20:43:16-05:00\" as \"2006-01-02T15:04:05Z07:00\": "+
+					"cannot parse \"20:43:16-05:00\" as \"2006\"",
+			),
+		},
+		"invalid RFC3339 - no time": {
+			RFC3339: timetypes.RFC3339{
+				StringValue: basetypes.NewStringValue("2023-07-25T"),
+			},
+			expectedFuncErr: function.NewArgumentFuncError(
+				0,
+				"Invalid RFC3339 String Value: "+
+					"A string value was provided that is not valid RFC3339 string format.\n\n"+
+					"Given Value: 2023-07-25T\n"+
+					"Error: parsing time \"2023-07-25T\" as \"2006-01-02T15:04:05Z07:00\": "+
+					"cannot parse \"\" as \"15\"",
+			),
+		},
+		"invalid RFC3339 - normal string": {
+			RFC3339: timetypes.RFC3339{
+				StringValue: basetypes.NewStringValue("notvalidrfc3339"),
+			},
+			expectedFuncErr: function.NewArgumentFuncError(
+				0,
+				"Invalid RFC3339 String Value: "+
+					"A string value was provided that is not valid RFC3339 string format.\n\n"+
+					"Given Value: notvalidrfc3339\n"+
+					"Error: parsing time \"notvalidrfc3339\" as \"2006-01-02T15:04:05Z07:00\": "+
+					"cannot parse \"notvalidrfc3339\" as \"2006\"",
+			),
+		},
+	}
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			resp := function.ValidateParameterResponse{}
+
+			testCase.RFC3339.ValidateParameter(
+				context.Background(),
+				function.ValidateParameterRequest{
+					Position: int64(0),
+				},
+				&resp,
+			)
+
+			if diff := cmp.Diff(resp.Error, testCase.expectedFuncErr); diff != "" {
 				t.Errorf("Unexpected diagnostics (-got, +expected): %s", diff)
 			}
 		})
