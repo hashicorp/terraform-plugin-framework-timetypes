7b8ad538db5264ecb66143ccecd9dbe47b26b1c3
Zane.Geiger <zgeiger@stsci.edu>
2024-12-20 19:43:46 -0500

    Add AtMost validator tests

---
+++ timetypes/godurationvalidator/at_most_example_test.go
@@ -0,0 +1,29 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package godurationvalidator_test
+
+import (
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes/godurationvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+)
+
+func ExampleAtMost() {
+	// Used within a Schema method of a DataSource, Provider, or Resource
+	_ = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"example_attr": schema.StringAttribute{
+				Required:   true,
+				CustomType: timetypes.GoDurationType{},
+				Validators: []validator.String{
+					// Validate duration value must be at most 5 seconds
+					godurationvalidator.AtMost(5 * time.Second),
+				},
+			},
+		},
+	}
+}
